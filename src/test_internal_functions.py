7393eaf1b5f941fd1ac3ac28d01e91fc9e90b0cb
krdelva <kevin.r.delva@gmail.com>
2025-03-30 19:35:20 -0400

    create internal_functions file, add extract_mardown image and link functions which use regex to extract the alt, text and url from markdown

---
+++ src/test_internal_functions.py
@@ -0,0 +1,21 @@
+import unittest
+
+from internal_functions import extract_markdown_images, extract_markdown_links
+
+
+class TestTextNode(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) woowow [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+if __name__ == "__main__":
+    unittest.main()
